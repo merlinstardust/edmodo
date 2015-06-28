d265b9a24be1ec2096cd622c713bfb7bbc0eae8d
Merlin Patterson <merlinpatt@gmail.com>
2015-06-28 18:29:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# edmodo
+
+An implementation of the Edmodo OAuth flow. See the [project
+page](https://www.meteor.com/accounts) on Meteor Accounts for more
+details.
---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'edmodo',
+  summary: 'Edmodo OAuth flow',
+  git: 'https://github.com/merlinpatt/edmodo',
+  version: '0.0.0',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.use('oauth2', ['client', 'server']);
+  api.use('oauth', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('templating', 'client');
+  api.use('underscore', 'client');
+  api.use('random', 'client');
+  api.use('service-configuration', ['client', 'server']);
+
+  api.export('Edmodo');
+
+  api.addFiles(
+    ['edmodo_configure.html', 'edmodo_configure.js'],
+    'client'
+  );
+
+  api.addFiles('edmodo_server.js', 'server');
+  api.addFiles('edmodo_client.js', 'client');
+});
